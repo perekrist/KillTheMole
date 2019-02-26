a2154a7e0c66c3dc2bbc1ecdd758b2c4a41abf29
perekrist <perekrist31@gmail.com>
2019-02-26 09:51:13 +0700

    Initial Commit

---
+++ killTheMoleTests/killTheMoleTests.swift
@@ -0,0 +1,34 @@
+//
+//  killTheMoleTests.swift
+//  killTheMoleTests
+//
+//  Created by Перегудова Кристина on 26.02.2019.
+//  Copyright © 2019 Перегудова Кристина. All rights reserved.
+//
+
+import XCTest
+@testable import killTheMole
+
+class killTheMoleTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
